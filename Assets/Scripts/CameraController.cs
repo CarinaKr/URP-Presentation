755bb3bf55dd18a0512477f931e596fdb3530aa5
CarinaKr <carina.kraff@t-online.de>
2019-10-27 16:03:25 +0100

    URP-Presentation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private int rendererCount = 1;
+
+    private UniversalAdditionalCameraData cameraData;
+    private UniversalRenderPipelineAsset universalRenderPipelineAsset;
+    private int currentRendereIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraData = GetComponent<UniversalAdditionalCameraData>();
+        cameraData.SetRenderer(currentRendereIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchCameraRenderer();
+        }
+    }
+
+    private void SwitchCameraRenderer()
+    {
+        currentRendereIndex = (currentRendereIndex + 1)%rendererCount;
+        cameraData.SetRenderer(currentRendereIndex);
+
+    }
+}
---
+++ Assets/UnityOcclusionDemoCompleted/Scripts/DistortionRendererFeatureDemo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class DistortionRendererFeatureDemo : ScriptableRendererFeature
+{
+    class DistortionRenderPass : ScriptableRenderPass
+    {
+        private int tempRTID, targetTextureID;
+        private bool isRenderingObject;
+        private ShaderTagId shaderTagID;
+
+        public DistortionRenderPass(DistortionSettings settings)
+        {
+            renderPassEvent = settings.renderPassEvent;
+            targetTextureID = Shader.PropertyToID(settings.textureID);
+            tempRTID = Shader.PropertyToID("_TempRT");
+            isRenderingObject = settings.isRenderingObject;
+            shaderTagID = new ShaderTagId(settings.shaderTagID);
+        }
+
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in an performance manner.
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            cmd.GetTemporaryRT(tempRTID, cameraTextureDescriptor);
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get();
+            cmd.Blit(BuiltinRenderTextureType.CameraTarget, tempRTID);
+            cmd.SetGlobalTexture(targetTextureID, tempRTID);
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+
+            if(isRenderingObject)
+            {
+                DrawingSettings drawingSettings = CreateDrawingSettings(shaderTagID, ref renderingData, SortingCriteria.CommonTransparent);
+                FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.transparent);
+                context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref filteringSettings);
+            }
+        }
+
+        /// Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            cmd.ReleaseTemporaryRT(tempRTID);
+        }
+    }
+
+    private DistortionRenderPass distortionRenderPass;
+    [SerializeField] private DistortionSettings settings;
+
+    [Serializable]
+    private class DistortionSettings
+    {
+        public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+        public string textureID = "_DistortionTexture";
+        public bool isRenderingObject = false;
+        public string shaderTagID = "DistortionPass";
+    }
+
+
+    public override void Create()
+    {
+        distortionRenderPass = new DistortionRenderPass(settings);
+
+        // Configures where the render pass should be injected.
+        //distortionRenderPass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(distortionRenderPass);
+    }
+}
+
+
